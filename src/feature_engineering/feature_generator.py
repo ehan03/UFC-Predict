cb45693a8b35de015fb24212993e59bf5dfb3191
ehan03 <eugeneh442@gmail.com>
2024-01-24 12:16:55 -0500

    create files

---
+++ src/feature_engineering/feature_generator.py
@@ -0,0 +1,12 @@
+# standard library imports
+
+# third party imports
+
+# local imports
+
+
+class FeatureGenerator:
+    """
+    Class for generating relevant features and outputting
+    an 'ML-ready' dataset
+    """
