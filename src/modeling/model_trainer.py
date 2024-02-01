2561c43e35c53422c5f94ce42e07674a1fc2dc7e
ehan03 <eugeneh442@gmail.com>
2024-02-01 15:11:51 -0500

    create placeholder files for CV and training

---
+++ src/modeling/model_trainer.py
@@ -0,0 +1,14 @@
+# standard library imports
+
+# third party imports
+
+# local imports
+
+
+class ModelTrainer:
+    """
+    Class for (re)training, validating, and saving the main UFC-Predict model
+    """
+
+    def __init__(self):
+        pass
